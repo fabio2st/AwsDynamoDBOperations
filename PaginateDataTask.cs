fb7c8c291721f78a40369ad233b3cf473ade4849
Administrator <Administrator@EC2AMAZ-41CHOV6>
2023-05-17 15:38:06 +0000

    Add project files.

---
+++ PaginateDataTask.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+namespace DynamoDBOperations
+{
+    class PaginateDataTask
+    {
+        public async Task Run()
+        {
+            var configSettings = ConfigSettingsReader<DynamoDBConfigSettings>.Read("DynamoDB");
+
+            try
+            {
+                var ddbClient = new AmazonDynamoDBClient();
+
+                var tableName = configSettings.TableName;
+                var pageSize = configSettings.PageSize;
+
+                Console.WriteLine("\nScanning with pagination...\n");
+
+                await QueryAllNotesPaginator(ddbClient, tableName, pageSize);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
+        async Task QueryAllNotesPaginator(IAmazonDynamoDB ddbClient, string tableName, int pageSize)
+        {
+            // TODO 6: Add code that creates a paginator and prints the returned items
+            var scanRequest = new ScanRequest()
+            {
+                TableName = tableName,
+                Limit = pageSize,
+                ProjectionExpression = "UserId, NoteId, Note"
+            };
+            var paginator = ddbClient.Paginators.Scan(scanRequest);
+            var page = 1;
+            await foreach (var item in paginator.Responses)
+            {
+                Console.WriteLine($"Starting page {page}");
+                Print(item.Items);
+                Console.WriteLine($"End of page {page++}\n");
+            }
+            // End TODO 6
+        }
+
+        void Print(IEnumerable<Dictionary<string, AttributeValue>> notes)
+        {
+            foreach (var note in notes)
+            {
+                var json = JsonSerializer.Serialize(new
+                {
+                    UserId = note["UserId"].S,
+                    NoteId = note["NoteId"].N.ToString(),
+                    Note = note["Note"].S
+                });
+
+                Console.WriteLine(json.ToString());
+            }
+        }
+    }
+}
